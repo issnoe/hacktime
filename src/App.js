5bf07010a6fba1c9065d9317cac75b9d414d41e0
Luis <ljasso@k-care.com>
2024-07-26 21:32:25 -0600

    docs(package): add scripts to build and publish
    - build use craco ? not sure the difference
    - package create the app for mac, I need to add windows script and do test
    - publish has alwas not sure that it those
    
    the other files and dependecies are from the first prof of concept

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
 import logo from './logo.svg';
-import './App.css';
+//import Realm from "realm";
+import "./App.css";
+
+// const app = new Realm.App({ id: "hacktime" }); // create a new instance of the Realm.App
+// async function run() {
+//   // login with an anonymous credential
+//   await app.logIn(Realm.Credentials.anonymous());
+
+//   const DogSchema = {
+//     name: "Dog",
+//     properties: {
+//       _id: "int",
+//       name: "string",
+//       age: "int",
+//     },
+//     primaryKey: "_id",
+//   };
+
+//   const realm = await Realm.open({
+//     schema: [DogSchema],
+//     sync: {
+//       user: app.currentUser,
+//       partitionValue: "myPartition",
+//     },
+//   });
+
+//   console.log(realm.path);
+//   console.log(realm);
+
+//   // The myPartition realm is now synced to the device. You can
+//   // access it through the `realm` object returned by `Realm.open()`
+
+//   // write to the realm
+// }
+// run().catch((err) => {
+//   console.error("Failed to open realm:", err);
+// });
 
 function App() {
   return (
