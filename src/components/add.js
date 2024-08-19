9fce2b422f58b6c1fd01a12fb2f9967620bbf8af
Luis <ljasso@k-care.com>
2024-08-19 12:35:56 -0600

    basic componnets

---
+++ src/components/add.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+const AddInput = ({ handleTask }) => {
+  const [description, setDescription] = useState("");
+  const [time, setTime] = useState(undefined);
+  return (
+    <div>
+      <input
+        type="text"
+        value={description}
+        name="description"
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      ></input>
+      <input
+        value={time}
+        type="number"
+        name="time"
+        onChange={(e) => {
+          setTime(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          const _task = {
+            description,
+            time: Number(time),
+            compleated: false,
+          };
+          handleTask(_task);
+          setDescription("");
+          setTime(0);
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+export default AddInput;
---
+++ src/components/list.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const List = ({ list, updateCompleated }) => {
+  return (
+    <ul>
+      {list.map((item, index) => {
+        const { description, compleated } = item;
+        return (
+          <li>
+            <input
+              type="checkbox"
+              value={compleated}
+              onChange={() => {
+                updateCompleated(index);
+              }}
+            ></input>
+            {description}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default List;
